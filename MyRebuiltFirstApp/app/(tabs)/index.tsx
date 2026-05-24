16119337736671dba78222b84b384951a5a20393
S <fairfieldmisery@gmail.com>
2026-05-24 11:10:59 +0800

    03 | LECTURE 3
    Interactive Profile Card submission

--- MyRebuiltFirstApp/app/(tabs)/index.tsx
+++ MyRebuiltFirstApp/app/(tabs)/index.tsx
@@ -1,18 +1,39 @@
-import { Image } from 'react-native';
-import { StyleSheet } from 'react-native';
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, Image, ScrollView, StyleSheet } from 'react-native';
 
 export default function App() {
-return (
-  <View style={s.screen}>
-    <Image
-      source={{ uri: 'https://i.pinimg.com/736x/e3/be/30/e3be3081d56e8f0e31dbef11a83273ea.jpg' }}
-      style={s.photo}
-    />
-    <Text style={s.name}>Melde May / Sei</Text>
-    <Text style={s.course}>Multimedia Arts • CS126-1</Text>
-    <Text style={s.bio}>Graphic designer, Dead by Daylight enthusiast, really enjoys noodles and soup a bit too much than normal.</Text>
-  </View>
+  const [name, setName] = useState('');
+
+  return (
+    <View style={s.screen}>
+  
+      {/* Profile picture */}
+      <Image
+        source={{ uri: 'https://i.pinimg.com/736x/e3/be/30/e3be3081d56e8f0e31dbef11a83273ea.jpg' }}
+        style={s.photo}
+      />
+  
+      {/* Name input */}
+      <TextInput
+        placeholder="Enter your name"
+        onChangeText={setName}
+        style={{
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+          width: 200,
+          backgroundColor:'#e9e9e9',
+          borderColor:'#878787',
+        }}
+      />
+  
+      {/* Greeting */}
+      <Text style={s.name}>
+        Hello, {name}!
+      </Text>
+  
+    </View>
   );
 }
 
@@ -21,6 +42,5 @@ const s = StyleSheet.create({
   screen: { flex:1, alignItems:'center', justifyContent:'center', backgroundColor:'#151515', padding:20 },
   photo: { width:120, height:120, borderRadius:60 },
   name: { fontSize:22, fontWeight:'bold', marginTop:12, color:'#ecdfdf' },
-  course: { fontSize:14, color:'#cf4897', textAlign:'center' },
   bio: { fontSize:14, color:'#c1699d', textAlign:'center' }
 });
